83ff68b3b21297df591d4db3749dce8782c8da83
Pablo Quiroga <jtdoe78@gmail.com>
2024-07-24 12:02:47 -0300

    Added BuilderPatern to Book DTO

--- src/main/java/com/pabloquiroga/essentials/books/Book.java
+++ src/main/java/com/pabloquiroga/essentials/books/Book.java
@@ -3,13 +3,33 @@ package com.pabloquiroga.essentials.books;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Data;
 
+@Builder
 @Data
 @Entity
 public class Book {
     @Id @GeneratedValue public Long id;
+    public String title;
     public String author;
     public String anio_lanzamiento;
     public String editorial;
+
+    public Book() {}
+
+    public Book(String title, String author, String anio_lanzamiento, String editorial) {
+        this.title = title;
+        this.author = author;
+        this.anio_lanzamiento = anio_lanzamiento;
+        this.editorial = editorial;
+    }
+
+    public Book(Long id, String title, String author, String anio_lanzamiento, String editorial) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.anio_lanzamiento = anio_lanzamiento;
+        this.editorial = editorial;
+    }
 }
